ded6ee5aeacf128a4cf73a36ee60745f229fa320
MicheleCasani <casanimichele@gmail.com>
2025-03-24 15:22:18 +0100

    [UPD] done exercise 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const myFunction = (array) => {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].charAt(0) === 'A') {
+            newArray.push(array[i]);
+        }
+    }
 
-
+    return newArray;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(myFunction(names));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
